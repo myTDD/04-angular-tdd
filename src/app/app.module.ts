6626545744267f4f88222132401818a370cf32c0
Pieter Siegers <pieter@coursetune.com>
2022-02-17 22:20:01 -0600

    fix component locations for initial app for level II

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewMessageFormComponent } from './src/components/new-message-form/new-message-form.component';
-import { MessageListComponent } from './src/components/message-list/message-list.component';
-import { HeaderComponent } from './src/components/header/header.component';
+import { NewMessageFormComponent } from './components/new-message-form/new-message-form.component';
+import { MessageListComponent } from './components/message-list/message-list.component';
+import { HeaderComponent } from './components/header/header.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
