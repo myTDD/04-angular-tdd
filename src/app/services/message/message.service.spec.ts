067f940b835a01f21583581d6d0a2e18dc5ebe34
Pieter Siegers <pieter@coursetune.com>
2022-02-21 19:59:50 -0600

    tweak the service test file

--- src/app/services/message/message.service.spec.ts
+++ src/app/services/message/message.service.spec.ts
@@ -5,13 +5,13 @@ import { Message } from 'src/app/models/message/message';
 
 type messageListResponseType = Array<Message>;
 
-// const dummyMessageListResponse: messageListResponseType = [];
+const dummyMessageListResponse: messageListResponseType = [];
 
-const dummyMessageListResponse: messageListResponseType = [
-  { userId: 1, id: 1, title: 'title 1', body: 'body 1' },
-  { userId: 2, id: 2, title: 'title 2', body: 'body 2' },
-  { userId: 3, id: 3, title: 'title 3', body: 'body 3' },
-];
+// const dummyMessageListResponse: messageListResponseType = [
+//   { userId: 1, id: 1, title: 'title 1', body: 'body 1' },
+//   { userId: 2, id: 2, title: 'title 2', body: 'body 2' },
+//   { userId: 3, id: 3, title: 'title 3', body: 'body 3' },
+// ];
 
 describe('MessageService', () => {
   let injector: TestBed;
